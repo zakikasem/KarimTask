bf4923d504990bc84a6960b7a245c8391622090f
zakikasem <zakikassem2014@gmail.com>
2022-04-19 09:37:44 +0200

    Initial Commit

---
+++ KarimTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KarimTask
+//
+//  Created by macintosh on 19/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
